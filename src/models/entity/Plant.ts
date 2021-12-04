e17f80f1d2ea7704c01c9781388dee0fc34ba485
Biamah <beatriz.souza.costa@outlook.com>
2021-12-04 11:05:34 -0300

    fix: ajusta model para receber campos vazios

--- src/models/entity/Plant.ts
+++ src/models/entity/Plant.ts
@@ -5,15 +5,15 @@ export class Plant {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({name: 'name', type: 'varchar'})
+    @Column({name: 'name', type: 'varchar', nullable: true })
     name: string;
 
-    @Column({name: 'description', type: 'varchar'})
+    @Column({name: 'description', type: 'varchar', nullable: true })
     description: string;
 
-    @Column({name: 'care', type: 'varchar'})
+    @Column({name: 'care', type: 'varchar', nullable: true })
     care: string;
 
-    @Column({name: 'image', type: 'varchar'})
+    @Column({name: 'image', type: 'varchar', nullable: true })
     image: string;
 }
