97a05ba76fae5b07f1dde3c8bba921e9255e72c0
Biamah <beatriz.souza.costa@outlook.com>
2021-12-04 11:44:50 -0300

    feat: cria edit plant

--- src/controllers/plant/index.ts
+++ src/controllers/plant/index.ts
@@ -1,3 +1,4 @@
+import { validate } from "class-validator"
 import { getRepository } from "typeorm"
 import { Plant } from "../../models/entity/Plant"
 
@@ -36,4 +37,36 @@ export const delPlants = async (request, response) => {
         return response.json({ message: "Essa planta não existe mais!"})
     }
     return response.json({ message: "Removido com sucesso!"})
+}
+
+export const editPlants = async (request, response) => {
+    const plantRepository = getRepository(Plant)
+    const plants = await plantRepository.findOne(request.params.id)
+
+    if (!plants) {
+        return response.json({ message: "Planta não encontrada!" })
+    }
+
+    if (request.body.name) {
+        plants.name = request.body.name
+    }
+
+    if (request.body.description) {
+        plants.description= request.body.description
+    }
+
+    if (request.body.care) {
+        plants.care = request.body.care
+    }
+
+    if (request.body.image) {
+        plants.image = request.body.image
+    }
+
+    const errors = await validate(plants)
+    if (errors.length > 0) {
+        return response.status(400).json(errors)
+    }
+    await plantRepository.save(plants)
+    return response.json({ message: "Planta alterada com sucesso!"})
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { itsWorks } from "./controllers"
-import { addPlant, delPlants, getAllPlants } from "./controllers/plant"
+import { addPlant, delPlants, editPlants, getAllPlants } from "./controllers/plant"
 
 export const defineRoutes = (app) => {
     app.get('/', itsWorks)
 
     app.get('/plants', getAllPlants)
     app.post('/plants', addPlant)
+    app.patch('/plants/:id', editPlants)
     app.delete('/plants/:id', delPlants)
 }
